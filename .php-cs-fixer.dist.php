9d1f97f4e2fabc831c0185352f4c030856d88039
Stoyan Dimitrov <ctorh23@abv.bg>
2026-03-04 21:41:24 +0200

    Clean-up comments in the PHP-CS-Fixer configuration file and adjust settings.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,19 +10,12 @@ return (new Config())
     ->setRules([
         '@auto' => true
     ])
-    // 💡 by default, Fixer looks for `*.php` files excluding `./vendor/` - here, you can groom this config
+    ->setCacheFile(__DIR__ . '/var/build/php-cs-fixer/.php-cs-fixer.cache')
     ->setFinder(
         (new Finder())
-            // 💡 root folder to check
-            ->in(__DIR__)
-            // 💡 additional files, eg bin entry file
-            // ->append([__DIR__.'/bin-entry-file'])
-            // 💡 folders to exclude, if any
-            // ->exclude([/* ... */])
-            // 💡 path patterns to exclude, if any
-            // ->notPath([/* ... */])
-            // 💡 extra configs
-            // ->ignoreDotFiles(false) // true by default in v3, false in v4 or future mode
-            // ->ignoreVCS(true) // true by default
+            ->in([
+                __DIR__ . '/src',
+                __DIR__ . '/tests',
+            ])
     )
 ;
