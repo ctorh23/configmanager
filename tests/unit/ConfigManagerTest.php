2dc05add1fa91a4bc2c4af536cee9c330f860c4f
Stoyan Dimitrov <ctorh23@abv.bg>
2026-03-04 18:45:53 +0200

    Add coverage information.

--- tests/unit/ConfigManagerTest.php
+++ tests/unit/ConfigManagerTest.php
@@ -6,9 +6,14 @@ namespace Ctorh23\ConfigManagerTests;
 
 use Ctorh23\ConfigManager\ConfigManager;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
 
+#[CoversClass(ConfigManager::class)]
 class ConfigManagerTest extends TestCase
 {
+    /**
+     * @covers ConfigManager::get()
+     */
     public function testGetReturnsExpectedValue(): void
     {
         $confMan = new ConfigManager(__DIR__ . '/config');
