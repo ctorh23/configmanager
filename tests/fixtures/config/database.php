1232a3833480c203ee1c40bfc3054672456a1368
Stoyan Dimitrov <ctorh23@abv.bg>
2026-03-08 17:27:42 +0200

    Fix coding style.

--- tests/fixtures/config/database.php
+++ tests/fixtures/config/database.php
@@ -11,9 +11,9 @@ return [
             'port' => 3306,
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-        ]
+        ],
     ],
     'migrations' => [
-        'table' => 'migrations'
-    ]
+        'table' => 'migrations',
+    ],
 ];
